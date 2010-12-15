3b9e4221b9c45d2bdf97c6d8f60f585116aa2c4a
zverok <zverok@zverok.(none)>
2010-12-15 19:06:32 +0200

    Logs and errors processing

--- app/clio.rb
+++ app/clio.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'archive/zip'
+require 'logger'
 
 require 'frf_client'
 
@@ -9,24 +10,39 @@ module Clio
 	INDEXES = [Models::DateIndex, Models::HashtagIndex, Models::AllIndex]
 
 	class App < Sinatra::Base
-		get '/' do
-			haml :index
-		end
-		
 		BASE_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 		RESULT_PATH = File.join(BASE_PATH, 'result')
 		STATIC_PATH = File.join(BASE_PATH, 'templates')
 		ARCHIVES_PATH = File.join(BASE_PATH, 'public', 'zip')
+
+		def App.log; 
+			@log ||= Logger.new(File.join(BASE_PATH, 'logs', 'clio.log'))
+		end
+
+        error do
+            e = env['sinatra.error']
+            App.log.error e.message + "\n\t" + e.backtrace.join("\n\t")
+
+			haml :error
+        end
+
+		get '/' do
+			haml :index
+		end
 		
 		post '/' do
 			user = feed = params[:username]
 			key = params[:key]
 		
 			save_path = File.join(RESULT_PATH, user, feed)
+			
+			App.log.info "Starting to extract #{user}/#{feed}"
 
 			# 1. extract feed, saving entry files and indexing entries to DB
 			Models::Index.filter(:user => user, :feed => feed).delete
 			indexes = INDEXES.map{|klass| klass.create(:user => user, :feed => feed, :created_at => Time.now)}
+
+			App.log.info "#{user}/#{feed}: indexes created"
 
 			FriendFeedClient.extract_feed(
 				:login => user, 
@@ -35,10 +51,14 @@ module Clio
 				:path => save_path,
 				:indexes => indexes)
 
+			App.log.info "#{user}/#{feed}: feed extracted"
+
 			# 2. save indexes to files
 			Models::Index.filter(:user => user, :feed => feed).each do |idx|
-				idx.save(File.join(save_path, 'data', 'indexes'))
+				idx.to_file(File.join(save_path, 'data', 'indexes'))
 			end
+			
+			App.log.info "#{user}/#{feed}: indexes saved"
 			
 			# 3. copy static content
 			Dir[File.join(STATIC_PATH, '**', '*.*')].each do |src|
@@ -46,10 +66,14 @@ module Clio
 				FileUtils.makedirs File.dirname(tgt)
 				FileUtils.cp src, tgt
 			end
+
+			App.log.info "#{user}/#{feed}: templates copied"
 			
 			# 4. zip
 			Archive::Zip.archive(File.join(ARCHIVES_PATH, "#{user}.zip"), save_path)
 			
+			App.log.info "#{user}/#{feed}: results archived"
+
 			# 5. show link to result
 			@link = "/zip/#{user}.zip"
 			haml :result
